dd262fbad48bfd688c3438e9433226372bab9bc9
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-01 01:00:49 -0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -6,7 +6,11 @@ from django.http import HttpResponse
 def index(request):
     now = datetime.now()
     html = f'''
+    <!DOCTYPE html>
     <html>
+        <head>
+            <title>Meu primeiro site em Django</title>
+        </head>
         <body>
             <h1>Hello from Vercel!</h1>
             <p>The current time is { now }.</p>
