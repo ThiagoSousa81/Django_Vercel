b18581ad8a44dd1560e5943452fa8fb362740e9a
Thiago Sousa <thiagosousa81@gmail.com>
2023-12-02 20:49:22 -0300

    §

--- example/views.py
+++ example/views.py
@@ -5,15 +5,15 @@ from django.http import HttpResponse
 
 def index(request):
     now = datetime.now()
-    html = f'''
+    html = f'''    
     <!DOCTYPE html>
     <html>
         <head>
             <title>Meu primeiro site em Django</title>
         </head>
         <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <h1>{ print('Minha aplicação Django') }</h1>
+            <p>A data de hoje é { now }.</p>
         </body>
     </html>
     '''
